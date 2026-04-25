1d06d5112c264377cddbfdcdf1f117feafbf0b45
Coulibaly Nonta <coulibalynonta@gmail.com>
2026-04-25 09:10:33 +0100

    Enable script to exit on error
    Add error handling to stop script on failure

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Set the main script to stop if any of the sub-scripts exits with a non-zero status
+set -e
+
 bash ./scripts/build_tools.sh
 bash ./scripts/kernel_build.sh
 bash ./scripts/firmware_build.sh
 sudo bash ./scripts/rootfs_build.sh ubuntu-desktop
-
